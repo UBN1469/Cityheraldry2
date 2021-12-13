322242e8c2dd3ac170b1a0a2d9149222f068de5e
UBN100 <5423410@mail.ru>
2021-12-13 13:59:43 +0300

    Сделал вызов фрагмента через getSupportfragmentmemeger  портретной ориентации

--- app/src/main/java/com/mygeekbranch/cityheraldry2/MainActivity.java
+++ app/src/main/java/com/mygeekbranch/cityheraldry2/MainActivity.java
@@ -2,6 +2,7 @@ package com.mygeekbranch.cityheraldry2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new CitiesFragment() )
+                    .commit();
+
+        }
+
     }
 }
